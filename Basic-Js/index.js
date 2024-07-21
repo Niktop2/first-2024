27db0725b52ecd948b12058abe57ec055dfd960a
Nikhil <nikhilltopre5@gmail.com>
2024-07-22 01:02:52 +0530

    javascript basic

---
+++ Basic-Js/index.js
@@ -0,0 +1,23 @@
+console.log("I am learning JavaScript");
+
+let myVar = "This is a variable";
+    console.log(myVar);
+
+
+     let a = 5;
+     let b = 6;
+     let c = a + 4;
+     console.log(c);
+     c = b + 2;
+     console.log(c);
+
+     let i = 5;
+     console.log(i);
+     i = i + 5;
+     i += 5;
+     console.log(i);
+
+     const pi = 3.14159265359;
+     let radius = 4;
+    //pi = 2;
+     console.log(pi * radius * radius);
