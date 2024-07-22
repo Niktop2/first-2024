1219fab64fc3fff5dbab0a76b38147af6e8f52d6
Nikhil <nikhilltopre5@gmail.com>
2024-07-22 10:32:59 +0530

    add content in index.js

--- Basic-Js/index.js
+++ Basic-Js/index.js
@@ -1,23 +1,30 @@
-console.log("I am learning JavaScript");
+// console.log("I am learning JavaScript");
 
-let myVar = "This is a variable";
-    console.log(myVar);
+// let myVar = "This is a variable";
+//     console.log(myVar);
 
 
-     let a = 5;
-     let b = 6;
-     let c = a + 4;
-     console.log(c);
-     c = b + 2;
-     console.log(c);
+//      let a = 5;
+//      let b = 6;
+//      let c = a + 4;
+//      console.log(c);
+//      c = b + 2;
+//      console.log(c);
 
-     let i = 5;
-     console.log(i);
-     i = i + 5;
-     i += 5;
-     console.log(i);
+//      let i = 5;
+//      console.log(i);
+//      i = i + 5;
+//      i += 5;
+//      console.log(i);
 
-     const pi = 3.14159265359;
-     let radius = 4;
-    //pi = 2;
-     console.log(pi * radius * radius);
+//      const pi = 3.14159265359;
+//      let radius = 4;
+//     //pi = 2;
+//      console.log(pi * radius * radius);
+
+console.log("Namaste");
+console.log("Namaskar");
+console.log("2+3");
+console.log(eval(2+5));
+
+
